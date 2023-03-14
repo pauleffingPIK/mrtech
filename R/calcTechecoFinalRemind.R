4a4a1ec8a8e3d3de2dcc0e9b049fea55b186649a
effingpaul <effingpaul@tu-berlin.de>
2023-03-14 18:16:53 +0100

    add secondary data step

---
+++ R/calcTechecoFinalRemind.R
@@ -0,0 +1,23 @@
+#' @title       calcTechecoFinalRemind
+#' @description Calculates final data for REMIND from secondary data
+#'
+#' @param source Data source to be used:
+#'                 Dataframe - secondary data
+#'
+#' @return MAgPIE object with calculated parameters
+#' @author Paul Effing
+#' @export
+
+calcTechecoFinalRemind <- function ()
+   {
+    # get magpie object from read-method
+    x <- calcOutput("TechecoSecondary", aggregate=FALSE)
+
+    # perform calculations
+    # -------
+
+    # optional: perform corrections on data
+    # x <- correctTecheco(x)
+
+    return(list(x=x, weight=x, isocountries=FALSE, description="Eletrolysis data based on variuos sources. It contains the following variables: CAPEX as inco0: Initial investment costs given in TrEUR/TWa; OPEX as omf: Variable operation and maintenance costs given in TrEUR(2015)/TWa energy production; Fixed as omv: Fixed operation and maintenace costs given as a fraction of investment costs; efficiency as eta: Conversion efficieny, i.e. energy output divided by energy input"))
+}
---
+++ R/calcTechecoSecondary.R
@@ -0,0 +1,23 @@
+#' @title       calcTechecoSecondary
+#' @description Calculates secondary data from primary data source
+#'
+#' @param source Data source to be used:
+#'                csv - techno-economic csv-file
+#'
+#' @return MAgPIE object with calculated parameters
+#' @author Paul Effing
+#' @export
+
+calcTechecoSecondary <- function (source = "csv")
+   {
+    # get magpie object from read-method
+    x <- readSource("Techeco", source)
+
+    # perform calculations
+    # -------
+
+    # optional: perform corrections on data
+    # x <- correctTecheco(x)
+
+    return(list(x=x, weight=x, isocountries=FALSE, description="Eletrolysis data based on variuos sources. It contains the following variables: CAPEX as inco0: Initial investment costs given in TrEUR/TWa; OPEX as omf: Variable operation and maintenance costs given in TrEUR(2015)/TWa energy production; Fixed as omv: Fixed operation and maintenace costs given as a fraction of investment costs; efficiency as eta: Conversion efficieny, i.e. energy output divided by energy input"))
+}
